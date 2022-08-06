00cf1491366b0d782bcacac1924f8b4deb850f5f
Shariful Islam <engr.shariful.cse@gmail.com>
2022-08-06 03:32:43 +0200

    Created another file for component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+const lucky = 7;
 ReactDOM.render(
   <div>
     <h1>My First React App</h1>
+    <h3>
+      Hello my first app {`${lucky}`} is awesome looking niceHello my first app
+      is awesome
+    </h3>
+    <ol>
+      <li>One</li>
+      <li>Tow</li>
+      <li>Three</li>
+    </ol>
+    <p>Your Lucky Number {lucky}</p>
   </div>,
   document.getElementById("root")
 );
